5e4538a7b105fb35b6cb818e06423c96755eb6b1
Anton Skshidlevsky <meefik@gmail.com>
2026-07-01 12:42:06 +0200

    chore: add i18n support with i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { HugeiconsIcon } from "@hugeicons/react";
-import { Mic01Icon } from "@hugeicons/core-free-icons";
-import { Button } from "@/components/ui/button";
+import "@/lib/i18n.ts";
+import { JoinView } from "@/views/join/JoinView";
+import { ConfView } from "@/views/conference/ConfView";
 
 export function App() {
-  return (
-    <div className="flex h-full w-full flex-col justify-center items-center gap-4">
-      <h1>Peerix Talk</h1>
-      <img src="/logo.svg" alt="Logo" className="h-8 w-8" />
-      <Button variant="outline" size="icon-lg">
-        <HugeiconsIcon icon={Mic01Icon} />
-      </Button>
-    </div>
-  );
+  return <JoinView />;
 }
 
 export default App;
---
+++ src/lib/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpBackend from "i18next-http-backend";
+
+i18n
+  .use(HttpBackend) // Loads translations from /public/locales
+  .use(LanguageDetector) // Detects user browser language
+  .use(initReactI18next) // Passes i18n down to react-i18next
+  .init({
+    fallbackLng: "en",
+    debug: import.meta.env.DEV, // Logs issues in development mode
+    interpolation: {
+      escapeValue: false, // React already safeguards against XSS
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+  });
+
+export default i18n;
